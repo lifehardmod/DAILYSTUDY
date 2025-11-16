bcb0d9b94a093d26a391121a879e4b1ca841e5e8
서주원 <kciimnida@gmail.com>
2025-11-17 01:55:19 +0900

    fix: 빌드 확인했음

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import StudyCheck from "./page/StudyCheck";
-import { Analytics } from "@vercel/analytics/next";
 
 const queryClient = new QueryClient();
 
@@ -10,7 +9,6 @@ function App() {
       <main className="mx-auto max-w-4xl px-4 pb-20 sm:px-8">
         <StudyCheck />
       </main>
-      <Analytics />
     </QueryClientProvider>
   );
 }
